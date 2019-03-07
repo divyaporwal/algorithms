9e61adf875becbe7b08079a4e4819d2e2b6b1906
divya porwal <porwaldivya8@gmail.com>
2019-03-06 23:44:33 -0700

    remove elements from duplicate elements from list

---
+++ linked_list/removeDuplicateElementsFromList/removeDuplicateElementsFromList.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        if(head == null) {
+            return head;
+        }
+        while(temp.next != null && temp != null) {
+            
+            if(temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
